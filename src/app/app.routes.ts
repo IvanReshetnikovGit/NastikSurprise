18c5b103522d0ce6adf7cdacbff6d52cf1b310a9
IvanReshetnikovGit <ivanreshet@icloud.com>
2025-08-21 19:53:26 +0000

    basic idea

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Day1 } from './pages/day1/day1';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'day1', component: Day1 }
+];
